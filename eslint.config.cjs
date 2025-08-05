d56727b338de74e798780337a819202b31744b7c
Brandon <brandon@tendency.me>
2025-08-05 13:37:23 -0500

    🚨 Run linting

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,38 +1,39 @@
-const {
-    defineConfig,
-} = require("eslint/config");
+const { defineConfig } = require('eslint/config');
 
-const globals = require("globals");
-const tsParser = require("@typescript-eslint/parser");
-const typescriptEslint = require("@typescript-eslint/eslint-plugin");
-const js = require("@eslint/js");
+const globals = require('globals');
+const tsParser = require('@typescript-eslint/parser');
+const typescriptEslint = require('@typescript-eslint/eslint-plugin');
+const js = require('@eslint/js');
 
-const {
-    FlatCompat,
-} = require("@eslint/eslintrc");
+const { FlatCompat } = require('@eslint/eslintrc');
 
 const compat = new FlatCompat({
     baseDirectory: __dirname,
     recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+    allConfig: js.configs.all,
 });
 
-module.exports = defineConfig([{
-    languageOptions: {
-        globals: {
-            ...globals.node,
+module.exports = defineConfig([
+    {
+        languageOptions: {
+            globals: {
+                ...globals.node,
+            },
+
+            parser: tsParser,
         },
 
-        parser: tsParser,
-    },
+        plugins: {
+            '@typescript-eslint': typescriptEslint,
+        },
 
-    plugins: {
-        "@typescript-eslint": typescriptEslint,
-    },
+        extends: compat.extends(
+            'eslint:recommended',
+            'plugin:@typescript-eslint/recommended'
+        ),
 
-    extends: compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"),
-
-    rules: {
-        "no-trailing-spaces": "error",
+        rules: {
+            'no-trailing-spaces': 'error',
+        },
     },
-}]);
+]);
